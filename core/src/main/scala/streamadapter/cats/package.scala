4d9a49ef872e1dbf3fbad88d71f5fa0607a4ef49
John Sullivan <john.sullivan.mscs@gmail.com>
2017-02-23 13:35:26 -0600

    solved my pre-effects problem with stroke of luck

--- core/src/main/scala/streamadapter/cats/package.scala
+++ core/src/main/scala/streamadapter/cats/package.scala
@@ -9,9 +9,16 @@ import java.io.Closeable
 package object cats {
 
   /** produces a publisher adapter from iterator generator to a cats-style `io.iteratee` enumerator */
-  implicit def iterGenToCatsEnumerator[F[_]](implicit F: Monad[F]) = {
+  implicit def iterGenToCatsEnumerator[F[_]](implicit F: Monad[F]) =
+    new IterGenToCatsEnumerator(F).adapter
+
+  /** contains an adapter wrapped with the type we are adapting to. exposing the `CatsEnumerator` type
+   * within this class in a non-anonymous way helps resolve some problems the compiler has equating
+   * the type returned by the adapter with the expected type.
+   */
+  class IterGenToCatsEnumerator[F[_]](F: Monad[F]) {
     type CatsEnumerator[A] = Enumerator[F, A]
-    new PublisherAdapter[IterGen, CatsEnumerator] {
+    val adapter = new PublisherAdapter[IterGen, CatsEnumerator] {
       def adaptPublisher[E](iterGen: IterGen[E]): Enumerator[F, E] = {
         new Enumerator[F, E] {
           final def apply[A](step: Step[F, E, A]): F[Step[F, E, A]] = {
@@ -32,11 +39,18 @@ package object cats {
     }
   }
 
-  // TODO
   /** produces a publisher adapter from a cats-style `io.iteratee` enumerator to iterator generator */
-  implicit def catsEnumeratorToIterGen[F[_]](implicit F: Monad[F]) = {
+  implicit def catsEnumeratorToIterGen[F[_]](implicit F: Monad[F]) =
+    new CatsEnumeratorToIterGen(F).adapter
+
+  /** contains an adapter wrapped with the type we are adapting from. exposing the `CatsEnumerator` type
+   * within this class in a non-anonymous way helps resolve some problems the compiler has equating
+   * the type returned by the adapter with the expected type.
+   */
+  class CatsEnumeratorToIterGen[F[_]](F: Monad[F]) {
     type CatsEnumerator[A] = Enumerator[F, A]
-    new PublisherAdapter[CatsEnumerator, IterGen] {
+    // TODO
+    val adapter = new PublisherAdapter[CatsEnumerator, IterGen] {
       def adaptPublisher[A](enumerator: Enumerator[F, A]): IterGen[A] = { () =>
         new Iterator[A] with Closeable {
           def hasNext = ???
