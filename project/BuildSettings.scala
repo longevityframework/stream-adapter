df31b6a1daf662ca0bc501b2f3b4e313b350d793
John Sullivan <john.sullivan.mscs@gmail.com>
2017-03-22 11:06:08 -0500

    correct version to 0.0-SNAPSHOT

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -6,7 +6,7 @@ object BuildSettings {
 
   val commonSettings = Defaults.coreDefaultSettings ++ Seq(
     organization := "org.longevityframework",
-    version := "0.0.0",
+    version := "0.0-SNAPSHOT",
     scalaVersion := Dependencies.scalaVersionString,
     crossScalaVersions := Seq("2.11.8", Dependencies.scalaVersionString),
     cancelable in Global := true)
