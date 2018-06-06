43a84142ae199fde80917f7056f0e0f5a308be10
John Sullivan <john.sullivan.mscs@gmail.com>
2018-06-05 19:11:56 -0500

    update version to 0.2.0

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -6,7 +6,7 @@ object BuildSettings {
 
   val commonSettings = Defaults.coreDefaultSettings ++ Seq(
     organization := "org.longevityframework",
-    version := "0.2-SNAPSHOT",
+    version := "0.2.0",
     scalaVersion := Dependencies.scalaVersionString,
     crossScalaVersions := Seq("2.11.12", Dependencies.scalaVersionString),
     cancelable in Global := true)
